b22876a17af733844f09788959d3ba7bb5542273
CormacDC <60583567+CormacDC@users.noreply.github.com>
2021-03-10 12:50:14 -0500

    initialized second activity

--- app/src/main/java/com/example/tipcalculator_conahan/MainActivity.java
+++ app/src/main/java/com/example/tipcalculator_conahan/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.tipcalculator_conahan;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,19 +11,21 @@ import android.widget.RadioButton;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText subtotal_field;
-    EditText size_field;
-    RadioButton no_tip;
-    RadioButton fifteen;
-    RadioButton twenty;
-    RadioButton twenty_five;
-    RadioButton custom;
-    EditText custom_field;
-    Button calculate;
+    private EditText subtotal_field;
+    private EditText size_field;
+    private EditText custom_field;
+    private Button calculate;
 
-    float subtotal;
-    int size;
-    float tip;
+    private double subtotal;
+    private int size;
+    private double tip;
+    private double tipPercent;
+    private boolean customTip;
+
+    private double total;
+    private double totalPerPerson;
+
+    private Intent i;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,19 +34,57 @@ public class MainActivity extends AppCompatActivity {
 
         subtotal_field = findViewById(R.id.subtotal_field);
         size_field = findViewById(R.id.size_field);
-        no_tip = findViewById(R.id.no_tip);
-        fifteen = findViewById(R.id.fifteen);
-        twenty = findViewById(R.id.twenty);
-        twenty_five = findViewById(R.id.twenty_five);
-        custom = findViewById(R.id.custom);
         custom_field = findViewById(R.id.custom_field);
         calculate = findViewById(R.id.calculate);
+
+        i = new Intent(this, MainActivity2.class);
 
         calculate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                subtotal = Double.parseDouble(subtotal_field.getText().toString());
+                size = Integer.parseInt(size_field.getText().toString());
+                if(customTip)
+                    tip = Double.parseDouble(custom_field.getText().toString());
+                else
+                    tip = tipPercent * subtotal;
 
+                total = subtotal + tip;
+                totalPerPerson = total / size;
+
+
+                startActivity(i);
             }
         });
     }
+
+    public void onRadioButtonClicked(View view) {
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+
+        // Check which radio button was clicked
+        switch(view.getId()) {
+            case R.id.no_tip:
+                if (checked)
+                    tipPercent = 0.00;
+                    break;
+            case R.id.fifteen:
+                if (checked)
+                    tipPercent = 0.15;
+                    break;
+            case R.id.twenty:
+                if (checked)
+                    tipPercent = 0.20;
+                    break;
+            case R.id.twenty_five:
+                if (checked)
+                    tipPercent = 0.25;
+                    break;
+            case R.id.custom:
+                if (checked)
+                    customTip = true;
+                    break;
+        }
+    }
+
 }
